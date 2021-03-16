e46140b3d461b5f182c662077f5977c61fad0bbe
ironfistlovepunch <79779590+ironfistlovepunch@users.noreply.github.com>
2021-03-16 15:20:13 +0900

    Delete 両面化.bat

--- 両面化.bat
+++
@@ -1,9 +0,0 @@
-REM ファイル編集
-set pypath=%~dp0
-rem pyファイルはbatファイルと同じフォルダに置くためbatのパス=pyパスである
-
-echo %pypath%
-echo %1
-
-for /f "usebackq" %%t in (`%pypath%pdf両面化.py "%1"`) do set work=%%t
-echo %work%
